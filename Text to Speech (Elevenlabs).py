f1b309869689b096c2f0db22a536ce0898bd66f8
WilliamKyaww <williamkyaw2003@gmail.com>
2023-12-19 22:43:15 +0000

    Only necessary files
    changed base folder to just the main files requried

--- Text to Speech (Elevenlabs).py
+++
@@ -1,9 +0,0 @@
-from elevenlabs import generate, play
- 
-audio = generate(
-  text="Hello! I'm Batman, delighted to make your acquaintance!",
-  voice="Bella",
-  model="eleven_monolingual_v1"
-)
- 
-play(audio)
--- speech_to_text.py
+++ speech_to_text.py
@@ -3,34 +3,24 @@ import pyttsx3
 
 r = sr.Recognizer()
 
-def record_text():
+def speech_input():
     while(1):
         try:
             with sr.Microphone() as source2:
                 r.adjust_for_ambient_noise(source2, duration=0.2)
-                print("I'm listening")
+                print("\033[93m Active \033[0m")
                 
                 audio2 = r.listen(source2)
                 MyText = r.recognize_google(audio2)
+                MyText = MyText.lower() 
                 
                 return MyText
         
         except sr.RequestError as e:
-            print("Could not request results; {0}".format(e))
+            print("\033[91m Could not request results; {0} \033[0m".format(e))
+            print()
             
         except sr.UnknownValueError:
-            print("Unknown error occurred")
+            print("\033[91m Unknown error occurred \033[0m")
+            print()
             
-
-def output_text(text):
-    f = open("Documents\\Project Clockwork\\output.txt","a")
-    f.write(text)
-    f.write("\n")
-    f.close
-    return
-
-while(1):
-    text = record_text()
-    output_text(text)
-    
-    print("Wrote text")
